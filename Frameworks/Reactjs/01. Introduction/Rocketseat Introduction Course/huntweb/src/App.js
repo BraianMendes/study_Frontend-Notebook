97e34af45d688f6842f1eb11f0e41a15b901f6c9
Braian Corrêa <braian.unifei@gmail.com>
2019-12-31 15:43:33 -0300

    Reactjs Study Rocketseat Course Finished

--- Frameworks/Reactjs/01. Introduction/Rocketseat Introduction Course/huntweb/src/App.js
+++ Frameworks/Reactjs/01. Introduction/Rocketseat Introduction Course/huntweb/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import Routes from './routes';
 
 import Header from './components/Header';
-import Main from './pages/main';
 
 const App = () => (
     <div className="App">
       <Header />
-      <Main />
+      <Routes />
     </div>
 ); 
 
