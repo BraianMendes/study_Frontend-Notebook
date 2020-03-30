1ccce7ce45d8e8eb5f3193eba3025c9bf85f09b2
BraianMendes <00.braian.dev@gmail.com>
2020-03-30 02:24:06 -0300

    Js Project about Classes and Inheritances in Codecademy

---
+++ Web Languages/Javascript/Projects/Advanced/Classes and Inheritance Media in Books,Movies and Songs/main.js
@@ -0,0 +1,90 @@
+class Media{
+  constructor(title){
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = [];
+  }
+  
+  get title(){
+    return this._title;
+  }
+  
+  get isCheckedOut(){
+    return this._isCheckedOut;
+  }
+  
+  get ratings(){
+    return this._ratings;
+  }
+  
+  set isCheckedOut(arg){
+    this._isCheckedOut = arg;
+  }
+  
+  toogleCheckOutStatus(){
+    if(this._isCheckedOut){
+      this._isCheckedOut = false;
+    } else {
+      this._isCheckedOut = true;
+    }
+  }
+  
+  getAverageRating(){
+    let ratingSum = this.ratings.reduce((currentSum, rating) => currentSum + rating, 0);
+    const lengthOfArray = this.ratings.length;
+    return ratingSum / lengthOfArray;
+  }
+  
+  addRating(rating){
+    this._ratings.push(rating);
+  }
+}
+
+class Book extends Media{
+  constructor(author, title, pages){
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  }
+  
+  get author(){
+    return this._author;
+  }
+  
+  get pages(){
+    return this._pages;
+  }
+}
+
+class Movie extends Media{
+  constructor(director, title, runTime){
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  }
+  
+  get director(){
+    return this._director;
+  }
+  
+  get runTim(){
+    return this._runTime;
+  }
+}
+
+const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
+
+historyOfEverything.toogleCheckOutStatus;
+console.log(historyOfEverything.isCheckedOut);
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+console.log(historyOfEverything.getAverageRating());
+
+const speed = new Movie('Jan de Bont', 'Speed', 116);
+speed.toogleCheckOutStatus;
+console.log(speed.isCheckedOut);
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+console.log(speed.getAverageRating());
