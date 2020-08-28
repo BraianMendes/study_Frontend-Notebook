a6519d6ed166dc4757e937a3c306bf5d5e09cbea
Braian <00.braian.dev@gmail.com>
2020-08-28 17:05:07 -0300

    Braian: Composite Component in Storybook

---
+++ 16. Design Systems/taskbox/src/components/TaskList.stories.js
@@ -0,0 +1,52 @@
+// src/components/TaskList.stories.js
+
+import React from 'react';
+
+import TaskList from './TaskList';
+import * as TaskStories from './Task.stories';
+
+export default {
+  component: TaskList,
+  title: 'TaskList',
+  decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>],
+};
+
+const Template = args => <TaskList {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  // Shaping the stories through args composition.
+  // The data was inherited from the Default story in task.stories.js.
+  tasks: [
+    { ...TaskStories.default.args, id: '1', title: 'Task 1' },
+    { ...TaskStories.default.args, id: '2', title: 'Task 2' },
+    { ...TaskStories.default.args, id: '3', title: 'Task 3' },
+    { ...TaskStories.default.args, id: '4', title: 'Task 4' },
+    { ...TaskStories.default.args, id: '5', title: 'Task 5' },
+    { ...TaskStories.default.args, id: '6', title: 'Task 6' },
+  ],
+};
+
+export const WithPinnedTasks = Template.bind({});
+WithPinnedTasks.args = {
+  // Shaping the stories through args composition.
+  // Inherited data coming from the Default story.
+  tasks: [
+    ...Default.args.tasks.slice(0, 5),
+    { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
+  ],
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  tasks: [],
+  loading: true,
+};
+
+export const Empty = Template.bind({});
+Empty.args = {
+  // Shaping the stories through args composition.
+  // Inherited data coming from the Loading story.
+  ...Loading.args,
+  loading: false,
+};
