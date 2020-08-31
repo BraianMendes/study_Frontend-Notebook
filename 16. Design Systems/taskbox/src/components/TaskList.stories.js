d8bbe24780cc99c60defec02ac056533712af441
Braian <00.braian.dev@gmail.com>
2020-08-31 13:28:09 -0300

    Braian: Adding Data to taskbox Storybook with Jest and Redux now

--- 16. Design Systems/taskbox/src/components/TaskList.stories.js
+++ 16. Design Systems/taskbox/src/components/TaskList.stories.js
@@ -2,28 +2,28 @@
 
 import React from 'react';
 
-import TaskList from './TaskList';
+import { PureTaskList } from './TaskList';
 import * as TaskStories from './Task.stories';
 
 export default {
-  component: TaskList,
+  component: PureTaskList,
   title: 'TaskList',
   decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>],
 };
 
-const Template = args => <TaskList {...args} />;
+const Template = args => <PureTaskList {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
   // Shaping the stories through args composition.
-  // The data was inherited from the Default story in task.stories.js.
+  // The data was inherited the Default story in task.stories.js.
   tasks: [
-    { ...TaskStories.default.args, id: '1', title: 'Task 1' },
-    { ...TaskStories.default.args, id: '2', title: 'Task 2' },
-    { ...TaskStories.default.args, id: '3', title: 'Task 3' },
-    { ...TaskStories.default.args, id: '4', title: 'Task 4' },
-    { ...TaskStories.default.args, id: '5', title: 'Task 5' },
-    { ...TaskStories.default.args, id: '6', title: 'Task 6' },
+    { ...TaskStories.Default.args.task, id: '1', title: 'Task 1' },
+    { ...TaskStories.Default.args.task, id: '2', title: 'Task 2' },
+    { ...TaskStories.Default.args.task, id: '3', title: 'Task 3' },
+    { ...TaskStories.Default.args.task, id: '4', title: 'Task 4' },
+    { ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
+    { ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
   ],
 };
 
