916f1035a601fe5d3b7e085a3df7c8b988350038
BraianMendes <00.braian.dev@gmail.com>
2022-05-19 04:11:40 -0300

    Fix Issues

--- 22_Design_Systems/taskbox/.storybook/preview.js
+++
@@ -1,8 +0,0 @@
-// .storybook/preview.js
-
-import '../src/index.css';
-
-// Configures Storybook to log the actions(onArchiveTask and onPinTask) in the UI.
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-};
