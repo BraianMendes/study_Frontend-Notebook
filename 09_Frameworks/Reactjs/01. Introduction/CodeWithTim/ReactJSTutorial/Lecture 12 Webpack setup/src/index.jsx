76d9005d61f103feff8b58b831e82a07c1669392
BraianMendes <00.braian.dev@gmail.com>
2022-05-19 03:44:42 -0300

    Fix Issues based on inclusion of funcionality from untrusted source

--- 09_Frameworks/Reactjs/01. Introduction/CodeWithTim/ReactJSTutorial/Lecture 12 Webpack setup/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import BabelTransformer from './components/babel-transformer';
-
-ReactDOM.render(
-  <BabelTransformer />,
-  document.getElementById('app')
-);
